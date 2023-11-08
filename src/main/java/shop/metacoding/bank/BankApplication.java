275b454cefbbe56bd5668909a212adbda1d7d560
Legday <chlwodud0327@naver.com>
2023-11-08 14:53:36 +0900

    Feat: SecurityConfig 세팅
    비밀번호 암호화를 위한 BCryptPasswordEncoder 클래스 Bean 등록
    JWT 필터는 추후에 등록
    filterChain 클래스 Bean 등록
    - HTTP iframe 허용안함
    - csrf 토큰검사 비활성화
    - CORS js요청 거부
    - jSessionId 는 서버에서 관리안함
    - form 태그를 통한 Login 허용안함
    - httpBasic 비활성화
    - /api/s/** 인증필요 , /api/admin/** 관리자만 허용, 나머지 URI 는 모두 허용

--- src/main/java/shop/metacoding/bank/BankApplication.java
+++ src/main/java/shop/metacoding/bank/BankApplication.java
@@ -2,6 +2,7 @@ package shop.metacoding.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
@@ -9,7 +10,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class BankApplication {
 
 	public static void main(String[] args) {
+		/* ConfigurableApplicationContext context = SpringApplication.run(BankApplication.class, args);
+		String[] beanDefinitionNames = context.getBeanDefinitionNames();
+		for (String beanDefinitionName : beanDefinitionNames) {
+			System.out.println("beanDefinitionName = " + beanDefinitionName);
+		}*/
 		SpringApplication.run(BankApplication.class, args);
+
 	}
 
 }
---
+++ src/main/java/shop/metacoding/bank/config/SecurityConfig.java
@@ -0,0 +1,62 @@
+package shop.metacoding.bank.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import shop.metacoding.bank.domain.user.UserEnum;
+
+@Slf4j
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        log.debug("BCryptPasswordEncoder Bean 등록");
+        return new BCryptPasswordEncoder();
+    }
+
+    // TODO : JWT 필터 등록이 필요함
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        log.debug("FilterChain 빈 등록");
+        http.headers().frameOptions().disable(); //HTTP <iframe> 허용안함
+        http.csrf().disable(); // enable 이면 postman 작동안함
+        http.cors().configurationSource(configurationSource()); // todo : CORS 는  JS 요청 거부(일단은 null 설정, 추후에 변경)
+
+        // jSessionId 를 서버쪽에서 관리안한다는 뜻
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        // react, 앱 요청을 받기 때문에 Form 으로 Login 을 안한다.
+        http.formLogin().disable();
+        // httpBasic 은 브라우저가 팝업창을 이용해서 사용자 인증을 진행하는 것을 막는다.
+        http.httpBasic().disable();
+
+        http.authorizeRequests()
+                .antMatchers("/api/s/**").authenticated() // 인증필요
+                .antMatchers("/api/admin/**").hasRole(""+UserEnum.ADMIN) // ROLE_ 안붙혀도됨
+                .anyRequest().permitAll(); // 나머지 요청은 허용
+
+        return http.build();
+    }
+
+    public CorsConfigurationSource configurationSource(){
+        log.debug("configurationSource cors 설정이 SecurityFilterChain 등록됨 ");
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedHeader("*"); // 모든 헤더허용
+        configuration.addAllowedMethod("*"); // GET, POST, PUT, DELETE (Javascript 요청 허용)
+        configuration.addAllowedOriginPattern("*"); // 모든 주소 허용
+        configuration.setAllowCredentials(true); // 클라이언트에서 쿠키 요청 허용
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // 어떤 주소가 요청이 와도 위에 설정들을 다 셋팅한다.
+
+        return source;
+    }
+}
