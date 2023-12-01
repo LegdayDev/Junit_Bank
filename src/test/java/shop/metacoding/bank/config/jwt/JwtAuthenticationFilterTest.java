8b0310f5318154edeea67442e27a0ab72b58f899
Legday <chlwodud0327@naver.com>
2023-12-01 17:24:25 +0900

    Feat: JwtAuthenticationFilter 테스트
    로그인 성공 테스트는 HTTP 상태코드와 JWT토큰이 알맞게 만들어졌는지, jsonPath 를 통해 응답 바디 데이터의 value 값을 검증한다.
    로그인 실패 테스트는 잘못된 데이터값으로 요청 데이터를 만들어서 실제 통신을 하여 HTTP 상태코드 401 이 응답하는지 검증한다.

---
+++ src/test/java/shop/metacoding/bank/config/jwt/JwtAuthenticationFilterTest.java
@@ -0,0 +1,90 @@
+package shop.metacoding.bank.config.jwt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.*;
+import org.springframework.transaction.annotation.Transactional;
+import shop.metacoding.bank.config.dummy.DummyObject;
+import shop.metacoding.bank.domain.user.UserRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static shop.metacoding.bank.dto.user.UserReqDto.*;
+
+@Transactional
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+class JwtAuthenticationFilterTest extends DummyObject {
+
+    @Autowired
+    private ObjectMapper om;
+    @Autowired
+    private MockMvc mvc;
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void setUp() throws Exception{
+        userRepository.save(newUser("ssar", "쌀"));
+    }
+
+    @Test
+    public void successfulAuthentication_test() throws Exception {
+        //given
+        LoginReqDto loginReqDto = new LoginReqDto();
+        loginReqDto.setUsername("ssar");
+        loginReqDto.setPassword("1234");
+
+        String requestBody = om.writeValueAsString(loginReqDto);
+        System.out.println("requestBody = " + requestBody);
+
+        //when
+        ResultActions resultActions = mvc.perform(
+                post("/api/login").
+                content(requestBody).
+                contentType(MediaType.APPLICATION_JSON));
+
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        String jwtToken = resultActions.andReturn().getResponse().getHeader(JwtVO.HEADER);
+
+        //then
+        resultActions.andExpect(status().isOk());
+        assertNotNull(jwtToken);
+        assertTrue(jwtToken.startsWith(JwtVO.TOKEN_PREFIX));
+        resultActions.andExpect(jsonPath("$.data.username").value("ssar"));
+
+    }
+
+    @Test
+    public void unsuccessfulAuthentication_test() throws Exception {
+        //given
+        LoginReqDto loginReqDto = new LoginReqDto();
+        loginReqDto.setUsername("ssar");
+        loginReqDto.setPassword("123434");
+
+        String requestBody = om.writeValueAsString(loginReqDto);
+        System.out.println("요청 데이터 requestBody = " + requestBody);
+
+        //when
+        ResultActions resultActions = mvc.perform(
+                post("/api/login").
+                        content(requestBody).
+                        contentType(MediaType.APPLICATION_JSON));
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("응답 데이터 responseBody : " + responseBody);
+
+        //then
+        resultActions.andExpect(status().isUnauthorized());
+    }
+
+}
